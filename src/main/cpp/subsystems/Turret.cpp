1d48576319ffe2a008ab982da2cc2dcc2532c08d
sharaneleven <sharaneleven@gmail.com>
2026-02-28 11:38:22 -0800

    mostly tested and logic works (need to test with throughbore)

--- src/main/cpp/subsystems/Turret.cpp
+++ src/main/cpp/subsystems/Turret.cpp
@@ -14,24 +14,15 @@ Turret::Turret() {
 
     turretMotor.GetConfigurator().Apply(turretConfigs);
     encoder.GetConfigurator().Apply(encoderConfigs);
-    encoderOffset = encoderConfigs.MagnetSensor.MagnetOffset.value();
 
 }
 
-void Turret::addToSetpoint(double addition){
-    setpoint += addition;
-}
-
-//stop motors
-void Turret::stop(){
-    turretMotor.StopMotor();
-}
-      
 //set speed
 void Turret::setSpeed(double spd){
     turretMotor.Set(spd);
     speed = spd;
 }
+
 void Turret::changeSpeed(double increment){
     speed += increment;
 }
@@ -40,111 +31,71 @@ double Turret::getSpeed(){
     return speed;
 }
 
+//for testing purposes without questnav
+void Turret::addToSetpoint(double addition){
+    setpoint += addition;
+}
 
-//angle of the turret found by calculating corrected encoder position and dividing by pulley ratio and mod by 360
-double Turret::getCurrentAngle() {
-    double encoderPos = turretMotor.GetPosition().GetValueAsDouble() * 360; //find pos in degrees
-    //double corrected = encoderPos - encoderOffset; //corrected value
-    double currentAngle = std::fmod(encoderPos/pulleyRatio, 360.0);//divide by pulley ratio to convert from angle of small wheel to angle of large wheel
+//stop motors
+void Turret::stop(){
+    turretMotor.StopMotor();
+}
+
+//angle of the turret found from the relative position of the motor
+double Turret::getCurrentMotorAngle() {
+    double motorPos = turretMotor.GetPosition().GetValueAsDouble() * 360; //find pos in degrees
+    double currentAngle = std::fmod(motorPos/pulleyRatio, 360.0);//pullyRatio = 44
     return currentAngle;
 }
 
-/*
-double Turret::findTurretAngle(){
-
-}
-*/
-void Turret::updateTurret(double targetAngle){
-    if (!isAtAngle(targetAngle)){
-        setAngle(targetAngle);
-    }
+//Not yet tested; We can get the relative value from the encoder, which makes life easier
+double Turret::getCurrentEncoderAngle() {
+    double encoderPos = encoder.GetPosition().GetValueAsDouble() * 360; //find pos in degrees
+    double currentAngle = std::fmod(encoderPos/tbTurretRatio, 360.0);//tb to turret ratio = 8.7777
+    return currentAngle;
 }
 
 void Turret::setAngle(double targetAngle) {
-    //double currentAngle = getCurrentAngle(); //get the current absolute angle
-    /*
-    if (fabs(currentAngle + targetAngle)>180){
-        resetTurretPosition(); //this will take care of if the turret rotates more that 180 deg.
-    }*/
-/*
-    double turnAngle = targetAngle - currentAngle; //find the angle the turret needs to turn
-    //decides which direction to turn
-    if (fabs(turnAngle) < 180){
-        frc::SmartDashboard::PutNumber("turn angle", turnAngle);
-        //turn to the desired position
-        turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t(turnAngle/360 * pulleyRatio)).WithSlot(0));
-        smallPulleyCounter += turnAngle/360 * pulleyRatio; //keep track of how much the small pulley turns
-    }
-    else{
-        //turn the opposite direction, change the turn angle
-        frc::SmartDashboard::PutNumber("turn angle", turnAngle);
-        turnAngle = (360-turnAngle) + currentAngle;
-        //turn to the desired position
-        turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t (-turnAngle/360 * pulleyRatio)).WithSlot(0));
-        smallPulleyCounter -= turnAngle/360 * pulleyRatio; //keep track of how much the small pulely turns
-    }
-    */
-   //turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t(targetAngle/360 * pulleyRatio)).WithSlot(0));
-
-//AFTER BASIC TESTS, TEST THIS LOGIC!!!
-
-    //double turretAngle = getCurrentAngle()*360/pulleyRatio;
     
     if (targetAngle > 180) {
         targetAngle = targetAngle - 360;
     } 
+    else if (targetAngle < -180) {
+        targetAngle = targetAngle + 360;
+    } 
     turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t(targetAngle/360 * pulleyRatio)).WithSlot(0));
-/*
-    double turretAngle = getCurrentAngle();
-    if (std::fabs(targetAngle + turretAngle) > 180){
-        turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t(-(360-targetAngle)/360 * pulleyRatio)).WithSlot(0));
-        smallPulleyCounter += -(360-targetAngle)/360 * pulleyRatio;
-    }
-    else if (std::fabs(targetAngle + turretAngle) < 180){
-        turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t(targetAngle/360 * pulleyRatio)).WithSlot(0));
-        smallPulleyCounter += targetAngle * pulleyRatio/360;
-    }
-    */
 
+//Not yet tested! I need to test the encoder values first, and this is just a backup when skipping happens
 
+//Should I use a while loop to make it stop when it hits the setpoint on the encoder value?
 
 /*
-    double turretAngle = getCurre
+    if (!isAtAngle(targetAngle)){
+        if (targetAngle >= 0){
+            setSpeed(0.1);
+        }
+        else if(targetAngle < 0){
+            setSpeed(-0.1);
+        }
+    }
 */
+
 }
 
 
 bool Turret::isAtAngle(double targetAngle) {
-    double encoderVal = fmod((pulleyRatio * targetAngle), 360);
-    double actualEncVal = getCurrentAngle();
-    if (std::fabs(encoderVal - actualEncVal) < 2) return true; // if the current angle is within 5 degrees of the target angle
+    if (targetAngle > 180) {
+        targetAngle = targetAngle - 360;
+    } 
+
+    else if (targetAngle < -180) {
+        targetAngle = targetAngle + 360;
+    } 
+    double actualEncVal = getCurrentMotorAngle();
+    if (std::fabs(targetAngle - actualEncVal) < 2) return true; // if the current angle is within 5 degrees of the target angle
     else return false;
 }
 
-//NOT GOOD
 void Turret::resetTurretPosition(){
-    double target = 0;
-    double currentAngle = getCurrentAngle();
-    //use the small pully counter to see how many turns the encoder did, convert that to degrees the turret turns
-    if(std::fabs(smallPulleyCounter*360/pulleyRatio)>180){
-        target = -currentAngle/360 * pulleyRatio; //set target angle
-        turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t (target)).WithSlot(0)); //rotate back the opposite way
-        smallPulleyCounter += target; //update small pulley counter
-    }
-    else {
-        target = (360-currentAngle)/360 * pulleyRatio; //set target angle
-        turretMotor.SetControl(positionVoltage.WithPosition(units::angle::turn_t (target)).WithSlot(0)); //rotate back the opposite way
-        smallPulleyCounter += target; //update small pulley counter
-    }
+    setAngle(0);
 }
-/*
-void Turret::correctionFunction(double target){
-    double currTBPos = getCurrentAngle(); //this should be the throughbore position when it it fixed
-    double targetTBPos = std::fmod(target*tbTurretrRatio, 360);
-    if (currTBPos )
-}
-*/
-
-//double Turret::getShortestAngle()
-
-
--- src/main/include/subsystems/Turret.h
+++ src/main/include/subsystems/Turret.h
@@ -14,56 +14,47 @@
 
 
 constexpr int encoderID = 60;
-constexpr int motorID = 13;
+constexpr int motorID = 0;
 
 constexpr double pulleyRatio = 44; //big wheel to small wheel (encoder) ratio
-constexpr double tbTurretrRatio = 8.77778;
+constexpr double tbTurretRatio = 8.77778;
 
-constexpr double kP = 0.8;
-constexpr double kI = 0.04;
+constexpr double kP = 1.05;
+constexpr double kI = 0.02;
 constexpr double kD = 0.10;
 
 
 class Turret : public frc2::Subsystem{
-   public:
+  public:
 
-     double setpoint = 0.0;
+    double setpoint = 0.0;
     double smallPulleyCounter = 0.0;
      
-     Turret();
-     void init(); 
-     void addToSetpoint(double addition);
+    Turret();
+    void setSpeed(double speed);
+    void changeSpeed(double increment);
+    double getSpeed();
+    void addToSetpoint(double addition);
+    void stop(); //stop motors
 
-     void stop(); //stop motors
-     void setSpeed(double speed);
-     void changeSpeed(double increment);
-     double getSpeed();
+    double getCurrentMotorAngle(); //use the gear ratio to calculate the current angle of the turret
+    double getCurrentEncoderAngle();
 
-        
-     double getCurrentAngle(); //use the gear ratio to calculate the current angle of the turret
-     void setAngle(double targetAngle);
-     bool isAtAngle(double targetAngle);
-     void resetTurretPosition();
-     double encoderCounter();
-     void updateTurret(double targetAngle);
-     void correctionFunction(double targetAngle);
-     double getShortestAngle(double targetAngle);
+    void setAngle(double targetAngle);
+    bool isAtAngle(double targetAngle);
 
-   private:
+    void resetTurretPosition();
+  
+  private:
 
-        ctre::phoenix6::hardware::TalonFX turretMotor{motorID};
-        ctre::phoenix6::configs::TalonFXConfiguration turretConfigs{};
+    ctre::phoenix6::hardware::TalonFX turretMotor{motorID};
+    ctre::phoenix6::configs::TalonFXConfiguration turretConfigs{};
 
-        ctre::phoenix6::hardware::CANcoder encoder{encoderID, "rio"};  
-        ctre::phoenix6::configs::CANcoderConfiguration encoderConfigs{}; 
+    ctre::phoenix6::hardware::CANcoder encoder{encoderID, "rio"};  
+    ctre::phoenix6::configs::CANcoderConfiguration encoderConfigs{}; 
 
-        ctre::phoenix6::controls::PositionVoltage positionVoltage{0_tr}; //turns 
+    ctre::phoenix6::controls::PositionVoltage positionVoltage{0_tr}; //turns 
 
-        double encoderOffset;
-
-        //since it is an absolute encoder that will go on the small pulley, need to keep track of how much the small pulley turns
-        //mainly for resetting the position of the turret when the angle exceeds 180
-       
-        double speed = 0.0;
+    double speed = 0.0;
 
 };
